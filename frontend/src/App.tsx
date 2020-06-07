c9ca89cb8a4b20da0281b52d6376ec53a091d4d1
Damian <damianjnc@gmail.com>
2020-06-07 19:04:58 +0100

    basic react setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {useQuery} from '@apollo/react-hooks'
+import {gql} from 'apollo-boost'
+
+function App() {
+  const {data, loading} = useQuery(gql`
+    {
+      hello
+    }
+
+  `)
+
+  if(loading) {
+    return <div>Loading...</div>
+  }
+
+  return (
+      <div>{JSON.stringify(data)}</div>
+  )
+}
+
+export default App;
